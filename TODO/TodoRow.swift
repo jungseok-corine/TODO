c66be9360f442d587db5a20f151400010b8b5fe7
jungseok-corine <nonehour@naver.com>
2025-10-30 10:12:44 +0900

    ##@Observable로 리팩토링
    1. @Published 불필요 - 모든 프로퍼티가 자동으로 관찰됨
    2. ObservableObject 프로토콜 불필요
    3. @StateObject 대신 @State 사용

---
+++ TODO/TodoRow.swift
@@ -0,0 +1,18 @@
+//
+//  TodoRow.swift
+//  TODO
+//
+//  Created by 오정석 on 30/10/2025.
+//
+
+import SwiftUI
+
+struct TodoRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodoRow()
+}
