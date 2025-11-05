bf79c72990732ea45fc55fe7e91b7f6613887dc7
오정석 <nonehour@naver.com>
2025-11-05 17:15:38 +0900

    **CoreData 기초** - NSManagedObject, NSManagedObjectContext 이해 - Entity 설계 및 Attributes 추가 - CRUD 구현 (Repository Pattern) - NSFetchRequest로 데이터 조회 - NSPredicate로 필터링
    **핵심 깨달음:**
    - CoreData는 UserDefaults보다 복잡하지만 강력함
    - Repository Pattern으로 추상화하면 나중에 API로 교체 가능
    - NSPredicate 문법이 SQL과 비슷

---
+++ TODO/Presentation/TodoList/PriorityBadge.swift
@@ -0,0 +1,18 @@
+//
+//  PriorityBadge.swift
+//  TODO
+//
+//  Created by 오정석 on 5/11/2025.
+//
+
+import SwiftUI
+
+struct PriorityBadge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PriorityBadge()
+}
